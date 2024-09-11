04150ba9214f930404a2edbb8babe78039f12579
Paul Cheng <chengyuejia@foxmail.com>
2024-09-11 16:25:14 +0800

    chore: add comments

--- src/keccakf.rs
+++ src/keccakf.rs
@@ -3,7 +3,9 @@ use crate::utils::*;
 use std::intrinsics::transmute_unchecked;
 
 // b=1600, aka round_1600
-pub fn permutation(a: [u64; WIDTH_IN_WORDS], round: usize) -> [u64; WIDTH_IN_WORDS] {
+//  The state is organized as an array of 5×5 lanes, each of length w∈{1,2,4,8,16,32,64}, and b=25w.
+//  When implemented on a 64-bit processor, a lane of Keccak-f[1600] can be represented as a 64-bit CPU word.
+pub fn round_1600(a: [u64; WIDTH_IN_WORDS], round: usize) -> [u64; WIDTH_IN_WORDS] {
     let mut a = a;
     // θ-Theta step
     // see more Algorithm 3 in Keecak-refrence-3.0
@@ -56,19 +58,19 @@ pub fn keccakf(input: [u64; WIDTH_IN_WORDS]) -> [u64; WIDTH_IN_WORDS] {
     let mut a = input;
 
     for i in 0..ROUNDS {
-        a = permutation(a, i);
+        a = round_1600(a, i);
     }
     a
 }
 
 #[cfg(test)]
 mod test {
-    use crate::keccakf::permutation;
+    use crate::keccakf::round_1600;
 
     #[test]
     fn test_permutation_test() {
         let inputs = [4_u64; 25];
-        let output = permutation(inputs, 1);
+        let output = round_1600(inputs, 1);
         println!("output: {:?}", output);
 
         println!("aaa: {:?}", 0x80 ^ 0);
